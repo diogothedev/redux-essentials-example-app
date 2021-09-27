96f20e3b93ca22a65f0bca6bff93b891f6c8b01b
Diogo Condêço <diogobcondeco@gmail.com>
2021-09-27 13:40:09 +0100

    Adding Author for Posts

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -3,6 +3,8 @@ import { useSelector } from 'react-redux'
 
 import { Link } from 'react-router-dom'
 
+import { PostAuthor } from './PostAuthor'
+
 export const SinglePostPage = ({ match }) => {
 	const { postId } = match.params
 
@@ -23,6 +25,7 @@ export const SinglePostPage = ({ match }) => {
 			<article className="post">
 				<h2>{post.title}</h2>
 				<p className="post-content">{post.content}</p>
+				<p><PostAuthor userId={post.user} /></p>
 				<Link to={`/editPost/${post.id}`} className="button">
 					Edit Post
 				</Link>
