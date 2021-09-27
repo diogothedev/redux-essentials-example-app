0e12e464ee7b3c9202f90e844285259eb024c93a
Diogo Condêço <diogobcondeco@gmail.com>
2021-09-27 13:59:20 +0100

    Storing Dates and Sorting Posts

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -3,15 +3,17 @@ import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 import { PostAuthor } from './PostAuthor'
+import { TimeAgo } from './TimeAgo'
 
 export const PostsList = () => {
 	const posts = useSelector(state => state.posts)
+	const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
 
-	const renderedPosts = posts.map(post => (
+	const renderedPosts = orderedPosts.map(post => (
 		<article className="post-excerpt" key={post.id}>
 			<h3>{post.title}</h3>
 			<p className="post-content">{post.content.substring(0, 100)}</p>
-			<p><PostAuthor userId={post.user} /></p>
+			<p><PostAuthor userId={post.user} /> <TimeAgo /></p>
 			<Link to={`/posts/${post.id}`} className="button muted-button">
 				View Post
 			</Link>
--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 import { PostAuthor } from './PostAuthor'
+import { TimeAgo } from './TimeAgo'
 
 export const SinglePostPage = ({ match }) => {
 	const { postId } = match.params
@@ -25,7 +26,7 @@ export const SinglePostPage = ({ match }) => {
 			<article className="post">
 				<h2>{post.title}</h2>
 				<p className="post-content">{post.content}</p>
-				<p><PostAuthor userId={post.user} /></p>
+				<p><PostAuthor userId={post.user} /> <TimeAgo /></p>
 				<Link to={`/editPost/${post.id}`} className="button">
 					Edit Post
 				</Link>
